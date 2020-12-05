589a05434a9e9242c7e197a9bb5458118a7c60d4
Eric E <eth_er@gmx.de>
2020-12-05 11:14:46 +0100

    day 5

---
+++ day5.py
@@ -0,0 +1,37 @@
+import math
+
+with open("input/day5", "r") as input:
+    data = input.read().split("\n")
+    data.pop()
+
+def parse(inputStr, idx, min, max):
+    if idx == len(inputStr):
+        if min == max:
+            return min
+        else:
+            raise Exception('oh no')
+
+    char = inputStr[idx]
+    avg = (max + min) / 2
+    if char == 'F' or char == 'L':
+        return parse(inputStr, idx + 1, min, math.floor(avg))
+    elif char == 'B' or char == 'R':
+        return parse(inputStr, idx + 1, math.ceil(avg), max)
+
+def getSeatID(entry):
+    row = parse(entry[:7], 0, 0, 127)
+    col = parse(entry[7:], 0, 0, 7)
+    return row * 8 + col
+
+def getMissingSeatID(seatIDs):
+    idx = 0
+    while idx + 1 < len(seatIDs):
+        if seatIDs[idx + 1] - seatIDs[idx] == 2:
+            return seatIDs[idx] + 1
+        idx += 1
+
+seatIDs = [getSeatID(entry) for entry in data]
+seatIDs.sort()
+print("Part 1: " + str(seatIDs[-1]))
+print("Part 2: " + str(getMissingSeatID(seatIDs)))
+    

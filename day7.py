9225897bab5f697f4594b3797a1696e2d75c390c
Eric E <eth_er@gmx.de>
2020-12-07 12:56:16 +0100

    day 7 refactor

--- day7.py
+++ day7.py
@@ -1,24 +1,26 @@
+from collections import namedtuple
 import re
 
 with open("input/day7", "r") as input:
     data = input.read().split("\n")
     data.pop()
 
+Bag = namedtuple('Bag', ['count', 'color'])
+
 bags = {}
 for entry in data:
     entry = entry.split(" bags contain ")
     matches = [re.match(r"^(\d+) (.+) bags?\.?$", bag) for bag in entry[1].split(", ")]
-    bags[entry[0]] = [(match[1] , match[2]) for match in matches if match]
+    bags[entry[0]] = [Bag(int(match[1]) , match[2]) for match in matches if match]
 
 def traverse(bag, targetBag):
     for containedBag in bag:
-        nextBag = containedBag[1]
-        if nextBag == targetBag or traverse(bags[nextBag], targetBag):
+        if containedBag.color == targetBag or traverse(bags[containedBag.color], targetBag):
             return True
     return False
 
 def traverseCount(bag):
-    return sum([int(containedBag[0]) + int(containedBag[0]) * traverseCount(bags[containedBag[1]]) for containedBag in bag])
+    return sum([containedBag.count + containedBag.count * traverseCount(bags[containedBag.color]) for containedBag in bag])
 
 pt1_bags = [color for color in bags if traverse(bags[color], "shiny gold")]
 

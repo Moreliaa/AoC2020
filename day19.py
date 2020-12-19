a05d823addf1c5c61ff77cfc596dccb29edd894a
Moreliaa <eth_er@gmx.de>
2020-12-19 21:37:14 +0100

    day 19

--- day19.py
+++ day19.py
@@ -10,22 +10,37 @@ for entry in data[0]:
     m = re.match(r"(\d+): (.+)", entry)
     d[m[1]] = [e.split(' ') for e in m[2].split(' | ')]
 
-def checkRule(key, msg, startIdx):
+def buildHugeRegex(key, isPt2):
     rules = d[key]
-    for r in rules:
-        idx = startIdx
-        result = True
+    regex = "("
+    for idx in range(len(rules)):
+        r = rules[idx]
         for num in r:
             if num[0] == '"':
-                if msg[idx] != num[1]:
-                    result = False
-                    break
-            elif not checkRule(num, msg, idx):
-                result = False
-                break
-            idx += 1
-        if result:
-            return result
-    return False
+                regex += num[1]
+            else:
+                regex += buildHugeRegex(num, isPt2)
+        if idx != len(rules) - 1:
+            regex += "|"
+    regex += ")"        
+    if isPt2 and key == '8':
+        regex += '+'
+    return regex
 
-print([checkRule("0", msg, 0) for msg in data[1]].count(True))
+def parts(isPt2):
+    rx = '^' + buildHugeRegex("0", isPt2) + '$'
+    matches = [re.match(rx, msg) for msg in data[1]]
+    return str(len(matches) - matches.count(None))
+
+print("Part 1: " + parts(False))
+
+# Part 2
+for i in range(2, 20): # arbitrary upper limit of 20, might need more depending on input
+    rule = []
+    for j in range(i):
+        rule.append('42')
+    for k in range(i):
+        rule.append('31')
+    d['11'].append(rule)
+
+print("Part 2: " + parts(True))
